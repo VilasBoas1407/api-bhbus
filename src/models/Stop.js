9b643e5aabe3aad8c263a6b25f4c2f27d22def63
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-05-20 14:31:49 -0300

    Adicionando Paradas de BH

---
+++ src/models/Stop.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const StopSchema = new mongoose.Schema({
+    ID_STOP : String,
+    NOME_STOP : String,
+    DESC_STOP : String,
+    LAT_STOP : String, //LATITUDE
+    LONG_STOP: String, //LONGITUDE
+    LOCATION_TYPE : String,
+    PARENT_STOP : String
+});
+
+module.exports = mongoose.model('Paradas',StopSchema);
