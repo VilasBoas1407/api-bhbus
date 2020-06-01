d7ea2026e3291ebe16488586fd5e8fe3cc263427
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-06-01 19:06:57 -0300

    Add CORS

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
+var cors = require('cors')
 const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+
 
 //Conexão com banco de dados
 mongoose.connect('mongodb+srv://apibh:apibh@cluster0-jxkcj.mongodb.net/OnTimeBus?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+app.use(cors());
 
 //Especificando o tipo de data que vai ser utilizado
 app.use(express.json());
